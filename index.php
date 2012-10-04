d563dc2100db662f4dd304e121d509c636ddea7b
Iain Mullan <iain.mullan@gmail.com>
2012-10-04 21:53:41 +0100

    add fork banner

--- index.php
+++ index.php
@@ -40,14 +40,12 @@
 		  var js, fjs = d.getElementsByTagName(s)[0];
 		  if (d.getElementById(id)) return;
 		  js = d.createElement(s); js.id = id;
-		  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=368540363214051";
+		  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
 		  fjs.parentNode.insertBefore(js, fjs);
 		}(document, 'script', 'facebook-jssdk'));</script>
 		
+		<?php include('fork.php'); ?>
 		
-        <!-- Add your site or application content here -->
-
-
 <div id="content">
 
 	<header>
